8959780adbd208f058bdbba3f6148f971376768f
Michał Knapik <michal.knapik@u2i.com>
2017-07-05 22:58:01 +0200

    Dummy classes

--- example/lib/fibonacci/iterative.rb
+++ example/lib/fibonacci/iterative.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 module Fibonacci
+  class Dummy
+
+  end
+
   class Iterative
+    class Boo
+
+    end
+
     def self.call(n)
       result = 1
       previous = 1
